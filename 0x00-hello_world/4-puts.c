a614ddc027f8dec3f774767fc5850aa4dcff4248
Muniratu Salihu <munirolly@gmail.com>
2023-04-20 15:27:05 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program about betty test
+ * Return: 0(Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
