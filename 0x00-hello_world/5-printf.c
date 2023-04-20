bdf806f36cb63e05f39f1d8f494d520faa68f100
Muniratu Salihu <munirolly@gmail.com>
2023-04-20 15:49:36 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program about betty test using printf
+ * main - A program about betty test using printf function
  * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
