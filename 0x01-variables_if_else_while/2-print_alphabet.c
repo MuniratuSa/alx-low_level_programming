4f62da475df33bd41bab84a3c196f73fe03c9b6f
Muniratu Salihu <munirolly@gmail.com>
2023-04-22 04:44:57 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char lc;
 
-	for (i = 0; i < 26; i++);
+	for (lc = 'a'; lc <= 'z'; lc++)
 	{
-		putchar(alp[i]);
+		putchar(lc);
 	}
 	putchar('\n');
 	return (0);
