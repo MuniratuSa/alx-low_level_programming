2eeab3a46b7a058e3f8543474cefe87c11fd2a1f
Muniratu Salihu <munirolly@gmail.com>
2023-04-23 10:18:50 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,8 @@ int main(void)
 	int num;
 	char la;
 
-	for (num = 0; num < 10; num++)
-	putchar(num % 10);
+	for (num = '0'; num <= 10; num++)
+	putchar(num);
 
 	for (la = 'a'; la <= 'f'; la++)
 	putchar(la);
