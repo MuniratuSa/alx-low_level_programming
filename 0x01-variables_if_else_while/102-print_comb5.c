af0d62714b9b4d9240d4187166365be56b3d5c97
Muniratu Salihu <munirolly@gmail.com>
2023-04-24 13:40:43 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,21 +6,22 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; j < 100; i++)
+	for (i = 0; i < 100; i++)
 {
 	for (j = 0; j < 100; j++)
 {
-	if (i < j)
+	if (j < i)
 {
-	putchar((i / 10) + 48);
-	putchar((i % 10) + 48);
+	putchar(i / 10 + '0');
+	putchar(i % 10 + '0');
 	putchar(' ');
-	putchar((j / 10) + 48);
-	putchar((j % 10) + 48);
+	putchar(j / 10 + '0');
+	putchar(j % 10 + '0');
 
-	if (i != 98 || j != 99)
+	if (i * 100 + j != 9899)
 {
 	putchar(',');
 	putchar(' ');
