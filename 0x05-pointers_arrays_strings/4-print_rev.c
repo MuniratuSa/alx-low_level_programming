09edd68e76f4ebf10c0d53191af8587f1cf0dfef
Muniratu Salihu <munirolly@gmail.com>
2023-05-02 14:35:07 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int m;
+
+
+	while (*s != '\n')
+{
+	longi++;
+	s++;
+}
+	s--;
+	for (m = longi; m > 0; m--)
+{
+	_putchar(s);
+	s--;
+}
+	_putchar('\n');
+}
