64b2f4ddd60d33f8ab20f1f3ac30ead121a8a333
Muniratu Salihu <munirolly@gmail.com>
2023-05-02 14:41:54 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,7 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_rev - prints a string, in reverse, followed by a new line
+ * print_rev - prints a string in reverse
  * @s: string
  * return: 0
  */
