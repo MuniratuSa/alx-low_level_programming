1cddd6c88c46528183352fc3df6cf62b877307cc
Muniratu Salihu <munirolly@gmail.com>
2023-05-02 14:56:47 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _puts -  prints a string, followed by a new line, to stdout
+ * _puts -  prints a string
  * @str: string to print
  */
 void _puts(char *str)
 {
-	for (*str != '\0';)
+	for (; *str != '\0'; str++)
 {
-	_putchar (*str++);
+	_putchar(*str);
 }
-	_putchar ('\n');
+	_putchar('\n');
 }
