050cfefe757e4b018b58e8ba64bbdbd3dad1c5e8
Muniratu Salihu <munirolly@gmail.com>
2023-05-02 13:15:27 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - write a function that takes a pointer to an int as parameter
+ * reset_to_98 - write a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
  * @n: input
  * Return: n
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Write a function that swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b);
+{
+	int n;
+
+	s = *a;
+	*a = *b;
+	*b = n;
+}
